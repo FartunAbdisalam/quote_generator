94ace2ee84754131efffd977ede94b59c4e0d89f
FartunAbdisalam <saidsadrian@gmail.com>
2024-09-24 19:52:54 +0600

    2nd

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const quoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//show loader
+function loading()
+{
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+//hide loader
+function complete()
+{
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+//generate quote
 function  newQuote()
 {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
 
   if(quote.author=='Anonymous')
@@ -26,20 +42,22 @@ function  newQuote()
   else{
     quoteText.classList.remove('long-quote');
   }
+  //set quote & hide loading
   quoteText.textContent = quote.text;
-  
+  complete();
 }
 
-
+//get quotes from the API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   
   try {
 
   const response = await fetch(apiUrl);
    apiQuotes = await response.json();
-
    newQuote();
+
   }
   catch(error) {
     //catch the error
@@ -55,4 +73,7 @@ function tweetQuote()
 quoteBtn.addEventListener('click',newQuote);
 twitterBtn.addEventListener('click',tweetQuote);
 
+
+
 getQuotes();
+
